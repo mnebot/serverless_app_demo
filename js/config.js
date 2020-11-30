4da8ae6b8f561de63bb91815272dd81bcf329d17
Marçal Nebot <mnebot@gmail.com>
2020-11-30 11:39:11 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_P6eHbuMOe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7th965l2e1dmani8f6d63ifsa1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
